8942972108eea27314daee3c135c6738f9d42556
MERIA-web <adogbagbemerveilles@gmail.com>
2026-07-29 12:15:28 +0100

    Autoriser le domaine Vercel dans CORS

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'https://lcs-funnel-front.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
